e2e6e7cb80e8d18fe9e40cc53420f6116ee6bcbf
dcmunhoz <dc.munhoz@hotmail.com>
2019-10-02 17:52:57 -0300

    Dia 3 - Edições front-end

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 const express  = require('express');
 const mongoose = require('mongoose');
 const routes   = require('./routes');
+const cors     = require('cors');
+const path     = require('path');
 
 const app = express();
 
@@ -10,7 +12,9 @@ mongoose.connect("mongodb+srv://omni:omni@cluster0-jiamz.mongodb.net/omnistack9?
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333, () => {
