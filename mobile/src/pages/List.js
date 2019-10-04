574d46194f59ee2003c02bb105503651725765d0
dcmunhoz <dc.munhoz@hotmail.com>
2019-10-03 21:44:02 -0300

    Dia 4 - Finalizando app

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,6 +1,49 @@
-import React from 'react';
-import { View } from 'react-native';
+import React , { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, Text, AsyncStorage, Image, StyleSheet } from 'react-native';
+
+import SpotList from '../components/SpotList'
+
+import logo from '../assets/logo.png'
 
 export default function List () {
-    return <View/>
+    const [techs, setTechs] =  useState([])
+
+    useEffect(() => {
+        async function loadTechs() {
+            
+            const response = await AsyncStorage.getItem('techs');
+            
+            const techsArray = response.split(',').map(techItem => techItem.trim())
+
+            setTechs(techsArray);
+
+        }
+
+        loadTechs();
+    }, []);
+
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image source={logo} style={styles.logo}/>
+
+            <ScrollView showsHorizontalScrollIndicator={false}>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />) }
+            </ScrollView>
+
+        </SafeAreaView>       
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    logo: {
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 10
+    }
+});
--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const api = axios.create({
-    baseURL: 'http://192.168.130.109:3333'
+    baseURL: 'http://192.168.1.15:3333'
 });
 
 export default api;
