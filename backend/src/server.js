eec818e11a79e44275956cd829d9cfedd6fa4073
dcmunhoz <dc.munhoz@hotmail.com>
2019-10-05 00:40:53 -0300

    Dia 5 - Configurando socket.io

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,13 @@ const routes   = require('./routes');
 const cors     = require('cors');
 const path     = require('path');
 
+const socket   = require('socket.io');
+const http     = require('http');
+
+ 
 const app = express();
+const server = http.Server(app);
+const io = socket(server);
 
 // Mongo connect
 mongoose.connect("mongodb+srv://omni:omni@cluster0-jiamz.mongodb.net/omnistack9?retryWrites=true&w=majority", {
@@ -12,11 +18,28 @@ mongoose.connect("mongodb+srv://omni:omni@cluster0-jiamz.mongodb.net/omnistack9?
     useUnifiedTopology: true
 });
 
+const connectedUsers = {};
+
+io.on('connection', socket => {
+
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next( );
+});
+
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333, () => {
+server.listen(3333, () => {
     console.log("AIRCNC API OPEN PORT 3333");
 });
--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -2,10 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
+import socketio from 'socket.io-client';
+
 import './style.css';
 
 export default function Dashboard(){
     const [spots, setSpots] = useState([]);
+
+    useEffect(() => {
+
+        const user_id = localStorage.getItem('user');
+        const socket = socketio('http://localhost:3333', {
+            query: { user_id }
+        });
+        
+    }, []);
 
     useEffect(() => {
         async function loadSpots() {
